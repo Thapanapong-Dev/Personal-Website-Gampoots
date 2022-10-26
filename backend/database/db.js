c82819a15cfd6389abcab75f31388573d342879e
Thapanapong Sinprommat <88784416+Thapanapong-Dev@users.noreply.github.com>
2022-10-26 21:39:12 +0700

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   db: "mongodb://localhost:27017/reactdb",
-  //   db: "mongodb+srv://gampoo:<GqN!4Mc49dS!9PH>@gampoots.vv5zidb.mongodb.net/?retryWrites=true&w=majority",
+  //   db: "[mongodb+srv://...]",
 };
