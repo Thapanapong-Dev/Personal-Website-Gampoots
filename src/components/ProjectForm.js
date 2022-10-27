cce16bb5e64e2fc03eb07abf7a8492475b5d76d3
KCcie <kthapnp@gmail.com>
2022-10-28 06:54:36 +0700

    form input picture link hard code

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Form } from "react-bootstrap";
 import "animate.css";
-import TrackVisibility from "react-on-screen";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import remove from "lodash/remove";
+
+const pictureStyle = {
+  button: {
+    margin: "0 0 0 0",
+  },
+  input: {
+    height: "2rem",
+    fontSize: "14px",
+    padding: "0px 26px",
+    margin: "0.5rem 0 0 0",
+  },
+};
 
 export const ProjectForm = () => {
   const curr = new Date();
@@ -20,7 +29,7 @@ export const ProjectForm = () => {
     videoLink: "",
     priority: "",
   };
-  const [toolsList, setToolsList] = useState(
+  const [tools, setTools] = useState(
     [
       "React.js",
       "React Native",
@@ -33,12 +42,50 @@ export const ProjectForm = () => {
       "HTML, CSS, JavaScript",
     ].sort()
   );
+
   const priorityList = ["Lowest", "Low", "Medium", "High", "Highest"];
   const [isDropdown, setIsDropdown] = useState(true);
   const [formDetails, setFormDetails] = useState(formInitialDetails);
   const [buttonText, setButtonText] = useState("Send");
   const [status, setStatus] = useState({});
   const [isTools, setIsTools] = useState(true);
+  const [countPic, setCountPic] = useState(0);
+  const [pictureLink, setPictureLink] = useState([]);
+
+  const uniqs = (a) =>
+    a.filter((e) => a.slice(a.indexOf(e) + 1).indexOf(e) === -1);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    savePictureLink();
+    // console.log("formDetails >>> ", formDetails);
+    // let response = await fetch("http://localhost:5000/contact", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify(formDetails),
+    // });
+    // setButtonText("Send");
+    // let result = await response.json();
+    // setFormDetails(formInitialDetails);
+    // if (result.code === 200) {
+    //   setStatus({ succes: true, message: "Message sent successfully" });
+    // } else {
+    //   setStatus({
+    //     succes: false,
+    //     message: "Something went wrong, please try again later.",
+    //   });
+    // }
+  };
+
+  const setformDefaultDetails = () => {
+    setFormDetails.name("");
+    setFormDetails.description("");
+    setFormDetails.date("");
+    setFormDetails.tools([]);
+  };
 
   const onFormUpdate = (category, value) => {
     setFormDetails({
@@ -47,55 +94,55 @@ export const ProjectForm = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code === 200) {
-      setStatus({ succes: true, message: "Message sent successfully" });
-    } else {
-      setStatus({
-        succes: false,
-        message: "Something went wrong, please try again later.",
-      });
-    }
-  };
-
-  const removeToolsItem = (tool) => {
-    remove(toolsList, (t) => t === tool);
-    setToolsList(toolsList.sort());
+  const addAndSaveIool = (item) => {
+    remove(tools, (i) => i === item);
+    setTools(tools.sort());
     setIsDropdown(false);
-    formDetails.tools.push(tool);
+    formDetails.tools.push(item);
   };
 
-  const pushToolItem = (tool) => {
-    remove(formDetails.tools, (t) => t === tool);
-    setFormDetails(formDetails);
+  const deleteIool = (item) => {
+    remove(formDetails.tools, (i) => i === item);
     setIsTools(false);
-    toolsList.push(tool);
-    toolsList.sort();
-  };
-  const x = () => {
-    console.log(toolsList);
+    tools.push(item);
+    tools.sort();
   };
 
-  // useEffect(() => {
-  //   toolsList.sort();
-  // }, [toolsList, formDetails.tools]);
+  const updatePictireLink = (e, idx) => {
+    const abc = {};
+    abc[idx] = e.target.value;
+    setPictureLink({ ...pictureLink, ...abc });
+    console.log("update pictureLink >> ", pictureLink);
+  };
+
+  const deletePictureLink = () => {
+    const keys = Object.keys(pictureLink);
+    const values = Object.values(pictureLink);
+    for (let i = keys.length - 1; i >= 0; i--) {
+      console.log("count: ", countPic);
+      console.log("keys: ", parseInt(keys[i]) + 1);
+      if (countPic === parseInt(keys[i]) + 1) {
+        console.log(pictureLink[("delete >> ", keys[i])]);
+        delete pictureLink[keys[i]];
+        break;
+      }
+    }
+    setCountPic(countPic - 1);
+    console.log("delete pictureLink >> ", pictureLink);
+  };
+
+  const savePictureLink = () => {
+    const values = Object.values(pictureLink);
+    for (let i = 0; i <= values.length; i++) {
+      if (values[i] !== undefined) formDetails.pictureLink.push(values[i]);
+    }
+    console.log("formDetails.pictureLink >> ", formDetails);
+  };
 
   return (
     <div>
       <h2>Get In Touch</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <Row>
           <Col size={12} sm={6} className="px-1">
             <input
@@ -106,6 +153,7 @@ export const ProjectForm = () => {
               onChange={(e) => onFormUpdate("name", e.target.value)}
             />
           </Col>
+
           <Col size={12} sm={6} className="px-1">
             <input
               type="date"
@@ -117,24 +165,27 @@ export const ProjectForm = () => {
               onChange={(e) => onFormUpdate("date", e.target.value)}
             />
           </Col>
+
           <Col size={12} sm={12} className="px-1">
             <div className="input-field">
               <div className="d-flex justify-content-between">
                 <div className="mb-2 d-inline">Languages & Frameworks</div>
                 <div className="d-inline dropdown">
-                  <button className="dropbtn">Choose</button>
+                  <button className="dropbtn" type="button">
+                    Choose
+                  </button>
                   <div className="dropdown-content">
-                    {toolsList.map((tool, idx) =>
+                    {tools.map((tool, idx) =>
                       isDropdown ? (
                         <button
                           key={idx}
                           type="button"
                           className="d-block dropdown-content-item"
                           onClick={() => {
-                            removeToolsItem(tool);
+                            addAndSaveIool(tool);
                           }}
                         >
-                          {toolsList[idx]}
+                          {tools[idx]}
                         </button>
                       ) : (
                         setIsDropdown(true)
@@ -150,7 +201,9 @@ export const ProjectForm = () => {
                       key={idx}
                       type="button"
                       className="tool-cross"
-                      onClick={() => pushToolItem(tool)}
+                      onClick={() => {
+                        deleteIool(tool);
+                      }}
                     >
                       {tool} ✖
                     </button>
@@ -161,16 +214,48 @@ export const ProjectForm = () => {
               </div>
             </div>
           </Col>
-          <Col size={12} sm={6} className="px-1">
-            <input
-              type="text"
-              className="input-field"
-              value={formDetails.projectLink}
-              placeholder="Project Link"
-              onChange={(e) => onFormUpdate("projectLink", e.target.value)}
-            />
+
+          <Col size={12} sm={12} className="px-1">
+            <div className="input-field">
+              <div className="d-flex justify-content-start">
+                <div className="mb-2 d-inline">Picture Link</div>
+                <button
+                  type="button"
+                  className="dropbtn d-inline mx-2"
+                  style={pictureStyle.button}
+                  onClick={() => {
+                    setCountPic(countPic + 1);
+                  }}
+                >
+                  add
+                </button>
+                <button
+                  type="button"
+                  className="dropbtn d-inline mx-2"
+                  style={pictureStyle.button}
+                  onClick={() => (countPic >= 1 ? deletePictureLink() : null)}
+                >
+                  remove
+                </button>
+              </div>
+
+              {Array.from(Array(countPic)).map((c, idx) => (
+                <div key={idx.toString()}>
+                  <input
+                    type="text"
+                    className="input-field input-field-size"
+                    style={pictureStyle.input}
+                    placeholder={`Picture link ${idx + 1}`}
+                    // value={pictureLink[idx]}
+                    // onInput={() => onEmpty()}
+                    onChange={(e) => updatePictireLink(e, idx)}
+                  />
+                </div>
+              ))}
+            </div>
           </Col>
-          <Col size={12} sm={6} className="px-1">
+
+          <Col size={12} sm={12} className="px-1">
             <input
               type="text"
               className="input-field"
@@ -179,13 +264,14 @@ export const ProjectForm = () => {
               onChange={(e) => onFormUpdate("videoLink", e.target.value)}
             />
           </Col>
+
           <Col size={12} sm={12} className="px-1">
             <input
               type="text"
               className="input-field"
-              value={formDetails.pictureLink}
-              placeholder="Picture Link"
-              onChange={(e) => onFormUpdate("pictureLink", e.target.value)}
+              value={formDetails.projectLink}
+              placeholder="Project Link"
+              onChange={(e) => onFormUpdate("projectLink", e.target.value)}
             />
           </Col>
           {/* <Col size={12} sm={12} className="px-1">
@@ -199,9 +285,14 @@ export const ProjectForm = () => {
                   <Form.Check
                     inline
                     label={priority}
+                    value={priority}
                     name="priorities"
                     type="radio"
                     id={`inline-${priority}-1`}
+                    placeholder="Priority"
+                    onChange={(e) => {
+                      onFormUpdate("priority", e.target.value);
+                    }}
                   />
                 </Col>
               ))}
@@ -217,7 +308,11 @@ export const ProjectForm = () => {
             ></textarea>
           </Col>
           <Col size={12} className="px-1">
-            <button className="form-submit" type="submit">
+            <button
+              className="form-submit"
+              onClick={handleSubmit}
+              type="submit"
+            >
               <span>{buttonText}</span>
             </button>
           </Col>
